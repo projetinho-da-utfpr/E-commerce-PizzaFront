39991c25b74c4ad2a4295e4529566cf1da66b4a7
GabrielMenPassos <gabriel.passos3105@gmail.com>
2024-04-10 22:49:33 -0300

    atualizações tamanho cardapio

--- pi/src/cardapio.jsx
+++ pi/src/cardapio.jsx
@@ -2,8 +2,9 @@ import React, { useState, useRef, useEffect } from 'react';
 
 function Cardapio({ menuItems }) {
   const [showAllItems, setShowAllItems] = useState(false);
+  const [quantities, setQuantities] = useState({}); 
   const boxContainerRef = useRef(null);
-  const firstRowItems = menuItems.slice(0, 3);
+  const firstRowItems = menuItems.slice(0, 4);
 
   const toggleMenu = () => {
     setShowAllItems(!showAllItems);
@@ -15,10 +16,18 @@ function Cardapio({ menuItems }) {
     menuElement.scrollIntoView({ behavior: 'smooth' });
   };
 
+  const handleQuantityChange = (id, event) => {
+    const value = event.target.value;
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [id]: value 
+    }));
+  };
+
   useEffect(() => {
     if (boxContainerRef.current) {
       const firstRowHeight = boxContainerRef.current.getBoundingClientRect().height;
-      boxContainerRef.current.style.maxHeight = showAllItems ? `${boxContainerRef.current.scrollHeight}px` : `550px`;
+      boxContainerRef.current.style.maxHeight = showAllItems ? `${boxContainerRef.current.scrollHeight}px` : `580px`;
     }
   }, [showAllItems]);
 
@@ -33,33 +42,48 @@ function Cardapio({ menuItems }) {
             <img src={menuItem.image} alt={menuItem.name} />
             <div className="name">{menuItem.name}</div>
             <form action="" method="post">
-              <input type="number" min="1" max="100" value="1" className="qty" name="qty" />
+              <input
+                type="number"
+                min="1"
+                max="100"
+                value={quantities[menuItem.id] || 0} 
+                onChange={(event) => handleQuantityChange(menuItem.id, event)} 
+                className="qty"
+                name="qty"
+              />
               <input type="submit" value="Adicionar ao carrinho" name="add_to_cart" className="btn" />
             </form>
           </div>
         ))}
-        {showAllItems && (
-          menuItems.slice(3).map((menuItem) => (
+        {showAllItems &&
+          menuItems.slice(4).map((menuItem) => (
             <div key={menuItem.id} className="box">
               <div className="price">R$<span>{menuItem.price}</span></div>
               <img src={menuItem.image} alt={menuItem.name} />
               <div className="name">{menuItem.name}</div>
               <form action="" method="post">
-                <input type="number" min="1" max="100" value="1" className="qty" name="qty" />
+                <input
+                  type="number"
+                  min="0"
+                  max="100"
+                  value={quantities[menuItem.id] || 0}
+                  onChange={(event) => handleQuantityChange(menuItem.id, event)}
+                  className="qty"
+                  name="qty"
+                />
                 <input type="submit" value="Adicionar ao carrinho" name="add_to_cart" className="btn" />
               </form>
             </div>
-          ))
-        )}
+          ))}
       </div>
 
       {!showAllItems ? (
         <button className="expand-button" onClick={toggleMenu}>
-          <i className="fa fa-angle-down"></i> 
+          <i className="fa fa-angle-down"></i>
         </button>
       ) : (
         <button className="close-button" onClick={handleCloseMenu}>
-          <i className="fa fa-angle-up"></i> 
+          <i className="fa fa-angle-up"></i>
         </button>
       )}
     </section>
